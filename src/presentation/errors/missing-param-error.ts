b59abfad7edc41467146c36c9b28e7b9046c049e
mateusprt <matnun95@gmail.com>
2024-08-10 12:07:10 -0300

    feat: ensure SignUpController returns 400 if an invalid email is provided

--- src/presentation/errors/missing-param-error.ts
+++ src/presentation/errors/missing-param-error.ts
@@ -1,6 +1,6 @@
 export class MissingParamError extends Error {
-  constructor(missingParamName: string) {
-    super(`Missing param: ${missingParamName}`)
+  constructor(paramName: string) {
+    super(`Missing param: ${paramName}`)
     this.name = 'MissingParamError'
   }
 }
